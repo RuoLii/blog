76198f612be8e1146f77348bea665fc5bf827ddc
HP <1069121098@qq.com>
2024-04-29 21:03:19 +0800

    first updated

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <NavBar/>
+  <MdEditor/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import "bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap/dist/css/bootstrap.min.css"
+import NavBar from "@/components/NavBar.vue";
+import MdEditor from "@/components/MdEditor.vue"
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: 'App',
+  components: {
+    MdEditor,
+    NavBar,
   }
 }
+</script>
+
+<style lang="scss">
+
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="wrapper">
+    <div class="container d-flex justify-content-center">
+      <div class="title">
+        <div class="title-info">
+          A Blog with Markdown
+          developed by Li
+        </div>
+        <div class="my-button">
+            <button type="button" class="btn btn-lg btn-primary-outline">登录</button>
+            <button type="button" class="btn btn-lg btn-primary-outline">注册</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  .wrapper{
+    background-image: linear-gradient(-225deg, #69EACB 0%, #EACCF8 48%, #6654F1 100%);
+  }
+
+  .title {
+    margin-top: 3vh;
+    text-align: center;
+    font-style: italic;
+    font-size: 2vh;
+    color: #6654F1;
+    font-family: 微软雅黑, serif;
+    font-weight: bolder;
+  }
+
+  .my-button {
+    display: flex;
+    justify-content: flex-end;
+  }
+  .my-button button {
+    margin-right: 30px;
+  }
+
+  .title-info {
+    padding: 1vh;
+    text-align: center;
+    font-size: 3vh;
+  }
+
+  .btn-primary-outline {
+    background-color: transparent;
+    border-color: #7161ef;
+    color: #6d64be;
+  }
+
+  .btn-primary-outline:hover {
+    color: white;
+    border-color: #290fe1;
+    transition-duration: 200ms;
+  }
+</style>
